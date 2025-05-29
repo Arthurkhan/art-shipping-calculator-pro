d077ff139bf1f3e95d0ace1ec101a2abc34c0f46
Arthurkhan <arthurokhan@gmail.com>
2025-05-29 14:18:57 +0700

    Fix sidebar hooks - add SidebarContext type export

--- src/lib/sidebar-hooks.ts
+++ src/lib/sidebar-hooks.ts
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { useIsMobile } from "@/hooks/use-mobile"
 
 export const SIDEBAR_COOKIE_NAME = "sidebar:state"
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
@@ -8,7 +7,7 @@ export const SIDEBAR_WIDTH_MOBILE = "18rem"
 export const SIDEBAR_WIDTH_ICON = "3rem"
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
-type SidebarContext = {
+export type SidebarContext = {
   state: "expanded" | "collapsed"
   open: boolean
   setOpen: (open: boolean) => void
